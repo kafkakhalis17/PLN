48108caf519f63fbef41b7ace55526b1464078de
Kafka khalis <khaliskafka17@gmail.com>
2019-09-08 21:08:18 +0700

    --

---
+++ application/views/v_masterpage.php
@@ -0,0 +1,15 @@
+<?php $this->load->view('template/header');?>
+   <div class="page" id="MasterPager">
+      <span class="page-title">Master Page</span>
+      <div class="box-container">
+        <div class="box box-grid" >
+            <div class="box-icon">
+                <img src="assets/css/man-user.svg" alt="">
+            </div>
+            <div class="box-title">
+                <span>Data Member</span>
+            </div>
+        </div>
+      </div>
+   </div>
+<?php $this->load->view('template/footer');?>
